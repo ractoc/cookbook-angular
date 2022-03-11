5ac47bab21c15c76fa5ee2932c26977741c9279f
ractoc <ractoc@ractoc.com>
2022-03-11 13:55:44 +0100

    deloy step fix for clean up of the old install

--- jenkins/JenkinsFile.groovy
+++ jenkins/JenkinsFile.groovy
@@ -11,8 +11,10 @@ pipeline {
 
     stage("Deploy Application"){
       steps{
+        echo 'make sure there is always something to clean'
+        sh 'touch /var/www/html/test.txt'
         echo 'cleanup previous install'
-        sh 'rm -R /var/www/html/* 1>/dev/null 2>&1'
+        sh "rm -R /var/www/html/* 1>/dev/null 2>&1"
         echo 'new install'
         sh "cp dist/* /var/www/html"
       }
