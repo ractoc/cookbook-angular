34689d06cba642e84eef798b17884053358f7bcd
ractoc <ractoc@ractoc.com>
2022-08-14 14:35:00 +0200

    aanpassing in de RouterModule die ervoor moet zorgen dat de pagina naar boven scrolled bij een pagina wijzizing.

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
   ],
   exports: [
     RouterModule
